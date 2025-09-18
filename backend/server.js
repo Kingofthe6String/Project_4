da06cf07f64cc6d7d29f15e58148bd111f40e7c9
Johnny-Rocket <spaciousguide@gmail.com>
2025-09-18 17:30:53 -0500

    routes and controllers folders

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,11 @@
 import { response } from "express";
 import express from "express";
-import noteRoutes from "./routes/noteRoutes.js";
+import noteRoutes from "../routes/noteRoutes.js";
+
+app.use("/api/notes", noteRoutes);
 
 const app = express();
 
-app.get("/api/notes", (req, res) => {
-  res.status(200).send("you got 5 notes");
-});
-
-app.post("/api/notes", (req, res) => {
-  res.status(200).json({ message: "note created successfully" });
-});
-
-app.put("/api/notes/:id", (req, res) => {
-  res.status(200).json({ message: "note update success" });
-});
-
-app.delete("/api/notes/:id", (req, res) => {
-  res.status(200).json({ message: "note delete success" });
-});
 app.listen(5001, () => {
   console.log("server started on port 5001");
 });
